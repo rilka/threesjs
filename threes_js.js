d5e485060013156dabb2abded088c5edd185d878
Angela Li <angelaconstanceli@gmail.com>
2014-02-17 23:32:45 -0500

    movement done (i think)

--- threes_js.js
+++ threes_js.js
@@ -28,19 +28,31 @@ if (Meteor.isClient) {
       return tile;
   }
 
-  // (I really don't understand computers)
+  // TODO: New tiles
+  // TODO: Refactor (this mess is embarrassing)
   $(window).on('keydown', function(e) {
     var tiles = Session.get("tiles");
     switch(e.which) {
       case 37: // Left
         console.log("left");
         for (var i = 0; i <= 3; i++) {
-          var stuck = false;
           for (var j = 0; j <= 3; j++) {
-            if (j == 0)
-              continue;
+            var stuck = false;
+            if (j == 0) continue; // Edge
+            else if (tiles[i][j] == 0) continue; // Empty space
+            else if (tiles[i][j - 1] != tiles[i][j]) {
+              if (tiles[i][j - 1] == 0) {
+                tiles[i][j - 1] = tiles[i][j];
+                tiles[i][j] = 0;
+              }
+              else if ((tiles[i][j - 1] == 1 && tiles[i][j] == 2) ||
+                       (tiles[i][j - 1] == 2 && tiles[i][j] == 1)) {
+                  tiles[i][j - 1] = 3;
+                  tiles[i][j] = 0;
+                }
+            }
             else {
-              tiles[i][j - 1] = tiles[i][j];
+              tiles[i][j - 1] *= 2; // Twins
               tiles[i][j] = 0;
             }
           }
@@ -50,12 +62,23 @@ if (Meteor.isClient) {
       case 39: // Right
         console.log("right");
         for (var i = 3; i >= 0; i--) {
-          var stuck = false;
           for (var j = 3; j >= 0; j--) {
-            if (j == 3)
-              continue;
+            var stuck = false;
+            if (j == 3) continue; // Edge
+            else if (tiles[i][j] == 0) continue; // Empty space
+            else if (tiles[i][j + 1] != tiles[i][j]) {
+              if (tiles[i][j + 1] == 0) {
+                tiles[i][j + 1] = tiles[i][j];
+                tiles[i][j] = 0;
+              }
+              else if ((tiles[i][j + 1] == 1 && tiles[i][j] == 2) ||
+                       (tiles[i][j + 1] == 2 && tiles[i][j] == 1)) {
+                tiles[i][j + 1] = 3;
+                tiles[i][j] = 0;
+              }
+            }
             else {
-              tiles[i][j + 1] = tiles[i][j];
+              tiles[i][j + 1] *= 2; // Twins
               tiles[i][j] = 0;
             }
           }
@@ -65,12 +88,23 @@ if (Meteor.isClient) {
       case 38: // Up
         console.log("up");
         for (var j = 0; j <= 3; j++) {
-          var stuck = false;
           for (var i = 0; i <= 3; i++) {
-            if (i == 0)
-              continue;
+            var stuck = false;
+            if (i == 0) continue; // Edge
+            else if (tiles[i][j] == 0) continue; // Empty space
+            else if (tiles[i - 1][j] != tiles[i][j]) {
+              if (tiles[i - 1][j] == 0) {
+                tiles[i - 1][j] = tiles[i][j];
+                tiles[i][j] = 0;
+              }
+              else if ((tiles[i - 1][j] == 1 && tiles[i][j] == 2) ||
+                       (tiles[i - 1][j] == 2 && tiles[i][j] == 1)) {
+                tiles[i - 1][j] = 3;
+                tiles[i][j] = 0;
+              }
+            }
             else {
-              tiles[i - 1][j] = tiles[i][j];
+              tiles[i - 1][j] *= 2; // Twins
               tiles[i][j] = 0;
             }
           }
@@ -80,12 +114,23 @@ if (Meteor.isClient) {
       case 40: // Down
         console.log("down");
         for (var j = 0; j <= 3; j++) {
-          var stuck = false;
           for (var i = 3; i >= 0; i--) {
-            if (i == 3)
-              continue;
+            var stuck = false;
+            if (i == 3) continue; // Edge
+            else if (tiles[i][j] == 0) continue; // Empty space
+            else if (tiles[i + 1][j] != tiles[i][j]) {
+              if (tiles[i + 1][j] == 0) {
+                tiles[i + 1][j] = tiles[i][j];
+                tiles[i][j] = 0;
+              }
+              else if ((tiles[i + 1][j] == 1 && tiles[i][j] == 2) ||
+                       (tiles[i + 1][j] == 2 && tiles[i][j] == 1)) {
+                tiles[i + 1][j] = 3;
+                tiles[i][j] = 0;
+              }
+            }
             else {
-              tiles[i + 1][j] = tiles[i][j];
+              tiles[i + 1][j] *= 2; // Twins
               tiles[i][j] = 0;
             }
           }
